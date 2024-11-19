1496f6a9e26ee305ecacb8bafda51da86636ef54
Elena Ollivin <fortnokses@gmail.com>
2024-11-19 22:17:52 +0300

    create new file

---
+++ .idea/controller/ProfileController.java
@@ -0,0 +1,19 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.javaclo_1_spring_boot.systemProfile.SystemProfile;
+
+@RestController
+@RequestMapping("/")
+public class ProfileController {
+    private SystemProfile profile;
+
+    public ProfileController(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("profile")
+    public String getProfile() {
+        return profile.getProfile();
+    }
+}
